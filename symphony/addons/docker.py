2ce45ca1bce3e5913dc07000166fd200efbddb3e
jirenz <zhujiren1155@hotmail.com>
2018-05-09 12:18:59 -0700

    temp progress

--- symphony/addons/docker.py
+++ symphony/addons/docker.py
@@ -1,9 +1,10 @@
 """
 Utilities of building a docker image by collecting several local directories
 """
-import tarfile
 from os.path import expanduser
 from pathlib import Path
+import shutil
+import docker
 
 
 class DockerBuilder:
@@ -12,64 +13,96 @@ class DockerBuilder:
     """
     def __init__(self,
                  dockerfile,
-                 needed_directories,
+                 context_directories,
                  temp_directory,
                  *, verbose=True):
         """
         Args:
         temp_directory(str): temporary directory to create the image in, created if not exist
         dockerfile(str): path to a existing docker file
-        needed_directories([{
-                            name:(str)
-                            path:(str)
-                            force_update:(bool)
-                            }
-                           ]): Directories under these paths will
-                               be zipped and put into build context
-        force_updates(str)
+        context_directories(dict): see self.configure_context()
         verbose(book): report progress of build
         """
-        self.temp_directory = Path(expanduser(temp_directory))
+        self.temp_directory = Path(expanduser(temp_directory)) / 'symphony_docker_build'
+        # Add a folder so that we won't to stupid things like `rm -rf /`
         self.dockerfile = Path(expanduser(dockerfile))
-        self.needed_directories = {}
-        for entry in needed_directories:
+        self.context_directories = {}
+        self.configure_context(context_directories)
+        self.verbose = verbose
+
+        self.client = docker.from_env()
+
+    def configure_context(self, context_directories):
+        """
+        Directories specified would be zipped and put into build context
+        Args:
+        context_directories([{
+                    name:(str)
+                    path:(str)
+                    force_update:(bool)
+                    }
+                   ]):
+        force_update: True to always rezip
+                      False to only rezip when the file is missing.
+        """
+        self.context_directories = {}
+        for entry in context_directories:
             entryname = entry['name']
-            if entryname in self.needed_directories:
+            if entryname in self.context_directories:
                 raise ValueError('Name {} is referred to by more than two paths: {}, {}' \
                                     .format(entryname, entry['path'],
-                                            self.needed_directories[entryname]))
-            self.needed_directories[entryname] = {
+                                            self.context_directories[entryname]))
+            self.context_directories[entryname] = {
                 'name': entryname,
                 'path': entry['path'],
                 'force_update': entry['force_update'],
             }
-        self.verbose = verbose
 
-    def build(self, image_name):
+    def build(self, tag):
         """
         Excecute build
         """
-        if self.verbose:
-            print("Using temporary build directory {}".format(str(self.temp_directory)))
+        self.print("Using temporary build directory {}".format(str(self.temp_directory)))
         self.temp_directory.mkdir(parents=True, exist_ok=True)
 
-        for k, v in self.needed_directories.items():
+        # Copy all temporary files
+        for k, v in self.context_directories.items():
             self.retrieve_directory(**v)
-        # Tar all temporary files
-        #
+
+        dockerfile = self.dockerfile.open('r')
+
+        response = self.client.build(path=str(self.temp_directory),
+                                     decode=True,
+                                     fileobj=dockerfile,
+                                     tag=tag)
+        print(response)
 
     def retrieve_directory(self, name, path, force_update):
         """
-        Zip a directory to temp_file_path
+        Copy a directory to temp_file_path
         Args:
             name(str): put files to self.temp_directory / name
             path(str): the directory to zip
             force_update(bool): True: always overwrite existing files in the build directory
                                 False: only overwrite if files in the build directory don't exist
         """
+        target_path = self.temp_directory / name
+        if target_path.exists():
+            if force_update:
+                self.print("removing cached files at {}".format(target_path))
+                shutil.rmtree(target_path)
+            else:
+                self.print("Using cached files at {}".format(target_path))
+                return
+        source = path
+        target = str(target_path)
+        self.print('cp -r {} {}'.format(source, target))
+        shutil.copytree(source, target)
+        return
+
+    def print(self, *args, **kwargs):
+        """
+        print depending on verbose settings
+        """
         if self.verbose:
-            print('Zipping directory at {}'.format(path))
-            
-
-
-
+            print(*args, **kwargs)
